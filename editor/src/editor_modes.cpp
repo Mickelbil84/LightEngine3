bc26b1d3257daf869fa29b657676dc25d0bad7fd
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2023-03-23 15:22:44 +0200

    Engine: set default tool to be move

--- editor/src/editor_modes.cpp
+++ editor/src/editor_modes.cpp
@@ -2,7 +2,7 @@
 
 LE3EditorModeState::LE3EditorModeState() :
     mode(LE3EditorModes::LE3EDITOR_IDLE),
-    gizmoMode(LE3EditorGizmoModes::LE3EDITOR_GIZMO_SELECT),
+    gizmoMode(LE3EditorGizmoModes::LE3EDITOR_GIZMO_MOVE),
     bCanLeftClick(true),
     bHoversGizmo(false)
 {
