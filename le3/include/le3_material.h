25613536aa6d68fc299ff050b2be8db87e18e78c
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2024-01-19 19:52:09 +0200

    Initial material struct

---
+++ le3/include/le3_material.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+// #include "le3_texture.h"
+#include "le3_shader.h"
+
+
+namespace le3 {
+    enum TEXTURE_INDEX {
+        DIFFUSE_TEXTURE_INDEX = 1,
+        SPECULAR_TEXTURE_INDEX = 2,
+        NORMAL_TEXTURE_INDEX = 3,
+        CUBEMAP_TEXTURE_INDEX = 4,
+        SHADOW_MAP_INDEX = 6
+    };
+
+    struct LE3Material {
+    public:
+        void apply();
+
+        ///////////////
+        // Members
+        ///////////////
+        LE3ShaderPtr shader = nullptr;
+
+        // Diffuse
+        glm::vec4 diffuseColor = glm::vec4(.8f, .8f, .8f, 1.f);
+        // LE3TexturePtr diffuseTexture = nullptr;
+        bool bUseDiffuseTexture = false;
+
+        // Specular
+        glm::vec3 specularColor = glm::vec3(1.f, 1.f, 1.f);
+        float specularIntensity = 1.f;
+        float shininess = 16.f;
+        // LE3TexturePtr specularTexture = nullptr;
+        bool bUseSpecularTexture = false;
+
+        // Normal
+        // LE3TexturePtr normalTexture = nullptr;
+        bool bUseNormalTexture = false;
+
+        // Cubemap
+        // LE3TexturePtr cubemap = nullptr;
+        float reflectionIntensity = 0.f;
+
+        // General settings
+        float tilingX=1.f, tilingY=1.f;
+    };
+}
---
+++ le3/src/le3_material.cpp
@@ -0,0 +1,6 @@
+#include "le3_material.h"
+using namespace le3;
+
+void LE3Material::apply() {
+    
+}
