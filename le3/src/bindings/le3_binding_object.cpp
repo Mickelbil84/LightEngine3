b7e0dcc74434f2be9d248f2dd676d2ce6bb30c91
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2024-02-02 18:58:17 +0200

    Object bindings

--- le3/src/bindings/le3_binding_object.cpp
+++ le3/src/bindings/le3_binding_object.cpp
@@ -2,7 +2,16 @@
 #include "le3_engine_systems.h"
 using namespace le3;
 
+static int bnd_LE3Object_getTransform(lua_State* L) {
+    int idx = 1;
+    LE3ObjectPtr* object = LE3GetScriptSystem().getUserType<LE3ObjectPtr>(idx++);
+    LE3Transform& transform = (*object)->getTransform();
+    LE3GetScriptSystem().pushUserType<LE3Transform>(&transform);
+    return 1;
+}
+
 static const luaL_Reg LE3ObjectLib[] = {
+    {"get_transform", bnd_LE3Object_getTransform},
     {NULL, NULL}
 };
 
