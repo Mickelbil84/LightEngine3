e2e699370cdc56ecd322356265d9840bb7a2efab
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-02-08 14:05:06 +0200

    Initial reparent code (transforms are still broken)

--- editor/src/commands/le3ed_com_reparent.cpp
+++ editor/src/commands/le3ed_com_reparent.cpp
@@ -1,18 +1,25 @@
 #include "commands/le3ed_com_reparent.h"
 using namespace le3;
 
-LE3EditorComReparent::LE3EditorComReparent(std::string objName, std::string newParentName) :
-    m_objName(objName), m_newParentName(newParentName) {
-    m_oldParentName = LE3GetActiveScene()->getObject(m_objName)->getName();
+LE3EditorComReparent::LE3EditorComReparent(std::vector<std::string> names) :
+    m_names(names) {
+    for (auto name : names) {
+        m_oldParents[name] = LE3GetActiveScene()->getObject(name)->getParent()->getName();
+    }
 }
 
 void LE3EditorComReparent::execute() {
-    LE3GetActiveScene()->getObject(m_objName)->reparent(
-        LE3GetActiveScene()->getObject(m_newParentName)
-    );
+    std::string parentName = m_names[m_names.size() - 1];
+    for (int idx = 0; idx < m_names.size() - 1; idx++) {
+        LE3GetActiveScene()->getObject(m_names[idx])->reparent(
+            LE3GetActiveScene()->getObject(parentName)
+        );
+    }
 }
 void LE3EditorComReparent::undo() {
-    LE3GetActiveScene()->getObject(m_objName)->reparent(
-        LE3GetActiveScene()->getObject(m_oldParentName)
-    );
+    for (auto [objName, oldParentName] : m_oldParents) {
+        LE3GetActiveScene()->getObject(objName)->reparent(
+            LE3GetActiveScene()->getObject(oldParentName)
+        );
+    }
 }
