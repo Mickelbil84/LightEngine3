09960f73ee4038f9cbcb7fcbd35c041eeaa7ece5
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-06-22 22:06:00 +0300

    Add playerstart class

--- le3/include/le3/gameplay/le3_playerstart.h
+++ le3/include/le3/gameplay/le3_playerstart.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include "scripting/le3_script_object.h"
+
 namespace le3 {
-    
+    const std::string LE3_PLAYERSTART_OBJECT_NAME = "__playerStart";
+    const std::string LE3_PLAYERSTART_DEFAULT_CLASS = "FreeCamPlayer";
+
+    class LE3PlayerStart : public LE3ScriptObject {
+        LE3PlayerStart(std::string classname = LE3_PLAYERSTART_DEFAULT_CLASS);
+        LE3_TYPE_RETURN(LE3PlayerStart)
+    };
 }
