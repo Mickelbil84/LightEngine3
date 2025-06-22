5eb0ee9dd444865a6963c0ae1a68b5dd74b12d41
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2025-06-22 21:54:14 +0300

    Create a seperate le3game executable

---
+++ le3game/src/main.cpp
@@ -0,0 +1,79 @@
+#include <le3.h>
+using namespace le3;
+
+#include <filesystem>
+
+#include <fmt/core.h>
+#include <NMB/NMB.h>
+
+#include "le3game_constants.h"
+
+class LE3Game : public LE3GameLogic {
+public:
+    void init() {
+        loadBoostrapConfig();
+        loadProjectArchives();
+        loadInitialScene();
+    }
+    void update(float deltaTime) {
+        LE3GetSceneManager().updateScenes(deltaTime);
+    }
+
+    void render() {
+        LE3GetActiveScene()->draw();
+    }
+
+    void renderDebug() {
+    }
+
+    void handleInput(LE3Input input) {
+    }
+
+private:
+    // Load the game bootstrap config file (which is in *.lua format)
+    void loadBoostrapConfig() {
+        std::string bootstrapContent;
+        try {
+            bootstrapContent = LE3DatBuffer::loadFromSystem(LE3_GAME_BOOTSTRAP_FILE).toString(); // The bootstrap file #define is defined in CMake!
+        } catch (std::runtime_error e) {
+            NMB::show("ERROR", "Could not read boostrap file! Closing...", NMB::Icon::ICON_ERROR);
+            exit(-1);
+        }
+        LE3GetScriptSystem().doString(bootstrapContent);
+        
+    }
+
+    // Load all *.dat files that are in the project root
+    void loadProjectArchives() {    
+        std::string projectPath = LE3GetConfig<std::string>("LE3GameConfig.ProjectPath");
+
+        // Preload also project & demos archives
+        std::string projectFilename = "le3proj.dat";
+        LE3GetDatFileSystem().addArchive("le3proj", fmt::format("{}/{}", projectPath, projectFilename));
+        LE3GetDatFileSystem().addArchive("demos", "demos.dat");
+
+        // Add the rest archives available
+        for (const auto& entry : std::filesystem::directory_iterator(projectPath)) {
+            if (entry.path().extension() == ".dat") {
+                if (entry.path().filename().string() != projectFilename)
+                    LE3GetDatFileSystem().addArchive(entry.path().stem().string(), fmt::format("{}/{}", projectPath, entry.path().filename().string()));
+            }
+        }
+        LE3GetAssetManager().reloadAssets();
+    }
+
+    // Load initial scene
+    void loadInitialScene() {
+        std::string initialSceneName = LE3GetConfig<std::string>("LE3GameConfig.InitialScene");
+        LE3GetPhysicsManager().reset();
+        LE3GetSceneManager().createScene("__main__", m_engineState, fmt::format("/le3proj/scenes/{}", initialSceneName));
+
+        LE3GetActiveScene()->addFreeCamera("camera");
+    }
+};
+
+int main() {
+    LE3Application app(std::make_unique<LE3Game>());
+    app.run();
+    return 0;
+}
