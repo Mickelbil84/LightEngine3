643d0b798dd5d7e3e595d9d5c964abcff5ee028d
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2026-01-19 15:11:44 +0200

    Small changes for examples

---
+++ examples/flappy-bird/scripts/game_manager.lua
@@ -0,0 +1,13 @@
+FlappyGameManager = LE3ScriptObject:new()
+function FlappyGameManager:init()
+    print("Hello World! <<from FlappyGameManager:init()>>")
+end
+
+function FlappyGameManager:update(deltaTime)
+end
+
+function FlappyGameManager:handleInput()
+end
+
+function FlappyGameManager:draw()
+end
