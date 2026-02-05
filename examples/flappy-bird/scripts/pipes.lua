c25f7b6e7677c6f1e2b57b8cb241ee74135bbb5d
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2026-02-05 14:53:57 +0200

    Add moving pipes

---
+++ examples/flappy-bird/scripts/pipes.lua
@@ -0,0 +1,65 @@
+FlappyPipes = LE3ScriptObject:new()
+
+function FlappyPipes:init()
+    self.shouldInit = true
+    self.speed = 1.0
+    self.numPipes = 5
+    self.xMin = -5
+    self.xMax = 5
+    self.yMin = 0.2
+    self.yMax = 1.2
+    self.zPos = 1.5
+    self.spacing = (self.xMax - self.xMin) / self.numPipes
+end
+
+function FlappyPipes:initAfterLoad()
+    self.pipes = {}
+    for i = 1, self.numPipes do
+        local up = LE3Scene.get_object_global("SM_pipe" .. i .. "_up")
+        local down = LE3Scene.get_object_global("SM_pipe" .. i .. "_down")
+
+        -- Store the base Y of each pipe so we preserve the gap between up/down
+        local tUp = LE3Object.get_transform(up)
+        local tDown = LE3Object.get_transform(down)
+        local _, yUpBase, _ = LE3Transform.get_position(tUp)
+        local _, yDownBase, _ = LE3Transform.get_position(tDown)
+
+        local x = self.xMax + (i - 1) * self.spacing
+        local y = self.yMin + math.random() * (self.yMax - self.yMin)
+
+        self.pipes[i] = {
+            up = up, down = down,
+            x = x, y = y,
+            yUpBase = yUpBase, yDownBase = yDownBase
+        }
+        self:applyPipePosition(i)
+    end
+end
+
+function FlappyPipes:applyPipePosition(i)
+    local pipe = self.pipes[i]
+    local tUp = LE3Object.get_transform(pipe.up)
+    local tDown = LE3Object.get_transform(pipe.down)
+    LE3Transform.set_position(tUp, pipe.x, pipe.yUpBase + pipe.y, self.zPos)
+    LE3Transform.set_position(tDown, pipe.x, pipe.yDownBase + pipe.y, self.zPos)
+end
+
+function FlappyPipes:update(deltaTime)
+    if self.shouldInit then
+        self:initAfterLoad()
+        self.shouldInit = false
+    end
+
+    for i = 1, self.numPipes do
+        local pipe = self.pipes[i]
+        pipe.x = pipe.x - self.speed * deltaTime
+        if pipe.x < self.xMin then
+            pipe.x = self.xMax
+            pipe.y = self.yMin + math.random() * (self.yMax - self.yMin)
+        end
+        self:applyPipePosition(i)
+    end
+end
+
+function FlappyPipes:draw()
+end
